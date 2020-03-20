cf00d2a82b08d5b16780f5054c2abda3ff7bdcd5
och5351 <dhcksgo5319@naver.com>
2020-03-20 14:10:28 +0900

    텍스트로 변경

--- ImmovablesApp/App.js
+++ ImmovablesApp/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { Text, View } from 'react-native';
 
-export default class Bananas extends Component {
+export default class HelloWorldApp extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
+      </View>
     );
   }
 }

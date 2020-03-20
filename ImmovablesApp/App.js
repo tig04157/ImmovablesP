abc787bda7b10019ac74814f52b40928f1a657a9
och5351 <dhcksgo5319@naver.com>
2020-03-20 15:20:39 +0900

    original

--- ImmovablesApp/App.js
+++ ImmovablesApp/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default class HelloWorldApp extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-        <Text>aa</Text>
-        <Text>ss</Text>
-
-        <Text>bb</Text>
-
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

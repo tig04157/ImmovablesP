22b1b8131deadd166d810e802eb393f3ad50b518
och5351 <dhcksgo5319@naver.com>
2020-06-10 15:32:13 +0900

    http

--- ImmovablesApp/http-common.js
+++ ImmovablesApp/http-common.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://192.168.0.24:3000/api",
-  //baseURL: "http://192.168.25.3:3000/api",
-  baseURL: "http://192.168.100.7:3000/api",
+  baseURL: "http://13.209.89.137:3000/api",
   headers: {    "Content-type": "application/json"
   }
 });

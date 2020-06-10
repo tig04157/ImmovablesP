4ce00f3c9fc67e2650ed0965ac6846c8a7b48bdc
och5351 <dhcksgo5319@naver.com>
2020-06-11 00:28:26 +0900

    change port

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -47,3 +48,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+var server = app.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + server.address().port);
+})

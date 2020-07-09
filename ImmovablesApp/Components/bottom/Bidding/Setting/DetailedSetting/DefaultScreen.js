4a5a348d3a5f50824cf4effcd994c075a14c9a7d
tig04157 <tig04157@naver.com>
2020-07-09 21:57:35 +0900

    detail in writemodal

--- ImmovablesApp/Components/bottom/Bidding/Setting/DetailedSetting/DefaultScreen.js
+++ ImmovablesApp/Components/bottom/Bidding/Setting/DetailedSetting/DefaultScreen.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react';
-import {TextInput,TouchableOpacity, View, Text, StyleSheet, Modal} from 'react-native';
+import {TextInput,TouchableOpacity, View, Text, StyleSheet, Modal, TouchableWithoutFeedback} from 'react-native';
 import {Container, Icon,Header} from 'native-base';
+import { ThemeContext } from 'react-navigation';
 
 export default class DefaultScreen extends PureComponent {
   constructor(props) {
@@ -13,12 +14,42 @@ export default class DefaultScreen extends PureComponent {
         ch1:0,
         priceshown: false,
         heightshown: false,
-        sizwshown: false
+        sizeshwon: false,
+        typeshown: false,
+        typebutton: 3,
+        typeresult:'',
+        priceresult1:'',
+        priceresult2:'',
+        heightresult:'',
+        sizeresult:'',
+        
     };
   }
+  priceresulthandle1 = (text) => {
+    if (/^\d+$/.test(text)) {
+      this.setState({priceresult1: text});
+    }
+  };
+  priceresulthandle2 = (text) => {
+    if (/^\d+$/.test(text)) {
+      this.setState({priceresult2: text});
+    }
+  };
+  Settypebuttonon = (type) => {
+    this.setState({typebutton: type});
+  };
   SetPriceModalVisible(visible) {
     this.setState({priceshown: visible});
-  }
+  };
+  SetTypeModalVisible(visible){
+    this.setState({typeshown:visible});
+  };
+  SetHeightModalVisible(visible){
+    this.setState({heightshown:visible});
+  };
+  SetSizeModalVisible(visible){
+    this.setState({sizeshwon:visible});
+  };
   chrent = (id) => {
     this.setState({rentId: id});
   };
@@ -31,6 +62,138 @@ export default class DefaultScreen extends PureComponent {
   chmove = (id) => {
     this.setState({moveId: id});
   };
+  SizeModal=()=>{
+    return(
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={this.state.sizeshwon}
+        onRequestClose={() => {
+        this.SetSizeModalVisible(!this.state.sizeshwon);
+    }}
+    backdrop={true}
+    >
+      <View style={{flex:1}}>
+        <Header style ={{justifyContent:'space-between'}}>
+          <Icon name='ios-arrow-back' onPress={()=>{this.SetSizeModalVisible(!this.state.sizeshwon);}}/>
+          <Text>평 수</Text>
+          <Text/>
+        </Header>
+        <View style={{flexDirection:'column', alignItems:'center', height:'80%'}}>
+          <View  style={{justifyContent: 'center',alignItems:'center',height:'10%'}}>
+            <Text>평 수를 입력하세요.</Text>
+          </View>
+          <View style={{width:'90%', justifyContent: 'flex-start',}}>
+            <Text style={{fontSize:15}}>평 수</Text>
+          </View>
+          <TextInput
+            keyboardType='numeric'          
+            onChangeText={(text)=>this.setState({sizeresult:text})}
+            style={{color:'#000', width:'90%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center'}}
+            placeholder='0'
+          >
+          </TextInput>
+        </View>
+        <View style={{alignItems:'center', justifyContent:'center', width:'100%'}}>
+          <TouchableOpacity 
+            onPress={()=>this.SetSizeModalVisible(!this.state.sizeshwon)}
+            style={{color:'#000', width:'90%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center', backgroundColor:"#004aff"}}>
+            <Text>입력</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      
+    </Modal>
+    )
+
+  }
+  HeightModal=()=>{
+    return(
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={this.state.heightshown}
+        onRequestClose={() => {
+        this.SetHeightModalVisible(!this.state.heightshown);
+    }}
+    backdrop={true}
+    >
+      <View style={{flex:1}}>
+        <Header style ={{justifyContent:'space-between'}}>
+          <Icon name='ios-arrow-back' onPress={()=>{this.SetHeightModalVisible(!this.state.heightshown);}}/>
+          <Text>거래종류</Text>
+          <Text/>
+        </Header>
+        <View style={{flexDirection:'column', alignItems:'center', height:'80%'}}>
+          <View  style={{justifyContent: 'center',alignItems:'center',height:'10%'}}>
+            <Text>층 수를 입력하세요.</Text>
+          </View>
+          <View style={{width:'90%', justifyContent: 'flex-start',}}>
+            <Text style={{fontSize:15}}>층 수</Text>
+          </View>
+          <TextInput
+            keyboardType='numeric'          
+            onChangeText={(text)=>this.setState({heightresult:text})}
+            style={{color:'#000', width:'90%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center'}}
+            placeholder='0'
+          >
+          </TextInput>
+        </View>
+        <View style={{alignItems:'center', justifyContent:'center', width:'100%'}}>
+          <TouchableOpacity 
+            onPress={()=>this.SetHeightModalVisible(!this.state.heightshown)}
+            style={{color:'#000', width:'90%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center', backgroundColor:"#004aff"}}>
+            <Text>입력</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      
+    </Modal>
+    )
+
+  }
+  TypeModal=()=>{
+    return(
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={this.state.typeshown}
+        onRequestClose={() => {
+        this.SetTypeModalVisible(!this.state.typeshown);
+    }}
+    backdrop={true}
+    >
+      <View style={{flex:1}}>
+        <Header style ={{justifyContent:'space-between'}}>
+          <Icon name='ios-arrow-back' onPress={()=>{this.SetTypeModalVisible(!this.state.typeshown);}}/>
+          <Text>거래종류</Text>
+          <Text/>
+        </Header>
+        <View style={{flexDirection:'column', alignItems:'center', height:'80%'}}>
+          <View  style={{justifyContent: 'center',alignItems:'center',height:'10%'}}>
+            <Text>거래종류를 입력하세요.</Text>
+          </View>
+          <TouchableOpacity 
+            onPress={()=>{this.Settypebuttonon(1); this.setState({typeresult:'매매'});}} style={this.state.typebutton === 1? styles.ontypebutton : styles.offtypebutton} >
+            <Text>매매</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={()=>{this.Settypebuttonon(2); this.setState({typeresult:'전.월세'});}} style={this.state.typebutton === 2? styles.ontypebutton : styles.offtypebutton}>
+            <Text>전.월세</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{alignItems:'center', justifyContent:'center', width:'100%'}}>
+          <TouchableOpacity 
+            onPress={()=>this.SetTypeModalVisible(!this.state.typeshown)}
+            style={{color:'#000', width:'90%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center', backgroundColor:"#004aff"}}>
+            <Text>입력</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      
+      </Modal>
+    )
+  }
   PriceModal=()=>{
     return(
       <Modal
@@ -43,21 +206,48 @@ export default class DefaultScreen extends PureComponent {
     backdrop={true}
     >
       <View style={{flex:1}}>
-        <Header style ={{justifyContent:'space-between'}}>
+        <Header style ={{justifyContent:'space-between', width:'100%'}}>
           <Icon name='ios-arrow-back' onPress={()=>{this.SetPriceModalVisible(!this.state.priceshown);}}/>
           <Text>가격</Text>
           <Text/>
         </Header>
-        <View style={{flexDirection:'column', alignItems:'center'}}>
-          <Text>
-            가격을 입력하세요.
-          </Text>
-          <Text>보증금(전세금)</Text>
-          <TextInput style={{color:'#000', width:'90%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center'}} placeholder='예)10 -> 10만원' placeholderTextColor='#000'/>
+        <View style ={{height:'80%', alignItems:'center'}}>
+          <View style={{height:'10%', justifyContent:'center', alignItems:'center'}}>
+            <Text>가격을 입력하세요.</Text>
+          </View>
+          <View style={{flexDirection:'column', alignItems:'flex-start', width:'90%'}}>
+            <View style={{flexDirection:'row'}}>
+              <Text style={{fontSize:15}}>보증금(전세금)</Text><Text style={{fontSize:15, color:'#BEBEBE'}}> 만원</Text>
+            </View>
+            <TextInput
+              keyboardType='numeric'
+              onChangeText={this.priceresulthandle1}
+              style={{color:'#000', width:'100%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center'}}
+              placeholder='0'/>
+          </View>
+          <View style={{width:'90%', height:'10%', alignItems: 'flex-start'}}>
+            <Text>전세는 전세금만 입력하세요.</Text>
+          </View>
+
+          <View style={{flexDirection:'column', alignItems:'flex-start', width:'90%'}}>
+            <View style={{flexDirection:'row'}}>
+              <Text style={{fontSize:15}}>월세</Text><Text style={{fontSize:15, color:'#BEBEBE'}}> 만원</Text>
+            </View>
+            <TextInput 
+            keyboardType='numeric'
+            onChangeText={this.priceresulthandle2}
+            style={{color:'#000', width:'100%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center'}}
+            placeholder='0'/>
+          </View>
         </View>
-        <View style={{flexDirection:'column', alignItems:'center'}}>
-          <Text>월세</Text>
-          <TextInput style={{color:'#000', width:'90%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center'}} placeholder='예)10 -> 10만원' placeholderTextColor='#000'/>
+        <View style={{alignItems:'center', justifyContent:'center', width:'100%'}}>
+          <TouchableOpacity
+            onPress={()=>{
+              this.SetPriceModalVisible(!this.state.priceshown);
+            }}
+            style={{color:'#000', width:'90%', height:40, borderWidth:0.5, borderRadius:5, justifyContent:'center', alignItems:'center', backgroundColor:"#004aff"}}>
+            <Text>입력</Text>
+          </TouchableOpacity>
         </View>
       </View>
       </Modal>
@@ -67,34 +257,57 @@ export default class DefaultScreen extends PureComponent {
     return (
         <Container style={styles.container}>
           {this.PriceModal()}
+          {this.TypeModal()}
+          {this.HeightModal()}
+          {this.SizeModal()}
           <View style={{ margin:3, width:'90%', height:'80%', borderWidth:0.5, borderRadius:5, alignItems:'center'}}>
             <View style={{width:'90%', borderBottomWidth:0.5}}>
               <Text style={{margin:15, fontSize: 15 }}>가장 중요한 정보이므로 정확히 입력하세요.</Text>
             </View>
             <View style={{width:'90%',height:'8%', justifyContent:'center', borderBottomWidth:0.5}}>
               <TouchableOpacity style={{ flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}
-                onPress={()=>this.SetPriceModalVisible(true)}>
+                onPress={()=>this.SetTypeModalVisible(true)}>
+                <Text>거래종류</Text>
+                <View style={{width:90, flexDirection:'row', alignItems:'center', justifyContent: 'space-between'}}>
+                  <Text>{this.state.typeresult===''?'입력하세요':this.state.typeresult}</Text>
+                  <Text/>
+                  <Icon name='ios-arrow-forward'/>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={{width:'90%',height:'8%', justifyContent:'center', borderBottomWidth:0.5}}>
+              <TouchableOpacity style={{ flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}
+                onPress={()=>{
+                  this.SetPriceModalVisible(true);
+                  this.setState({priceresult1:'', priceresult2:''});
+                }}>
                 <Text>가격</Text>
-                <View style={{ flexDirection:'row', alignItems:'center'}}>
-                  <Text>입력하세요</Text>
+                <View style={{width:90, flexDirection:'row', alignItems:'center', justifyContent: 'space-between'}}>
+                <Text>{this.state.priceresult2===''?this.state.priceresult1===''?'입력하세요':'전세'+this.state.priceresult1+'만 원':this.state.priceresult1==='입력하세요'?'':'월세'+this.state.priceresult1+'만 원 / '+this.state.priceresult2+'만 원'}</Text>
                   <Icon name='ios-arrow-forward'/>
                 </View>
               </TouchableOpacity>
             </View>
             <View style={{width:'90%', height:'8%', justifyContent:'center', borderBottomWidth:0.5}}>
-              <TouchableOpacity style={{ flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+              <TouchableOpacity 
+                onPress={()=>{
+                  this.SetHeightModalVisible(true);
+                }}
+                style={{ flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
                 <Text>층 수</Text>
-                <View style={{ flexDirection:'row', alignItems:'center'}}>
-                  <Text>입력하세요</Text>
+                <View style={{ width:90, flexDirection:'row', alignItems:'center', justifyContent: 'space-between'}}>
+                  <Text>{this.state.heightresult===''?'입력하세요':this.state.heightresult+'층'}</Text>
                   <Icon name='ios-arrow-forward'/>
                 </View>
               </TouchableOpacity>
             </View>
             <View style={{width:'90%', height:'8%', justifyContent:'center', borderBottomWidth:0.5}}>
-              <TouchableOpacity style={{ flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
+              <TouchableOpacity 
+                onPress={()=>this.SetSizeModalVisible(true)}
+                style={{ flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
                 <Text>방 크기</Text>
-                <View style={{ flexDirection:'row', alignItems:'center'}}>
-                  <Text>입력하세요</Text>
+                <View style={{ width:90, flexDirection:'row', alignItems:'center', justifyContent: 'space-between'}}>
+                  <Text>{this.state.sizeresult===''?'입력하세요':this.state.sizeresult+'평'}</Text>
                   <Icon name='ios-arrow-forward'/>
                 </View>
               </TouchableOpacity>
@@ -184,5 +397,25 @@ const styles = StyleSheet.create({
     },
     black:{
         color:'black'
+    },
+    ontypebutton:{
+      color:'#000', 
+      width:'90%', 
+      height:40, 
+      borderWidth:0.5, 
+      borderRadius:5, 
+      justifyContent:'center', 
+      alignItems:'center',
+      backgroundColor:'#004aff'
+    },
+    offtypebutton:{
+      color:'#000', 
+      width:'90%', 
+      height:40, 
+      borderWidth:0.5, 
+      borderRadius:5, 
+      justifyContent:'center', 
+      alignItems:'center',
+      
     }
 });
